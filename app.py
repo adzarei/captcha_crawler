5ed7acd503d39e9cabf0465719324c21847dff8a
adzarei <adzarei@inf.upv.es>
2019-01-15 13:51:49 +0100

    Error handling + usage clue

--- app.py
+++ app.py
@@ -10,10 +10,14 @@ def post_url_for_ocr():
     try:
         url = request.json['image_url']
         output = process_image(url)
-        return jsonify({"captcha": output})
+        return jsonify({
+            "captcha": output
+        })
 
     except:
-
+        return jsonify({
+            "error": "try {'image_url': 'https://asdfg'}"
+        })
 
 if __name__ == '__main__':
     app.run()
